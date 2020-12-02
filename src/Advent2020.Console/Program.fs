67f6b9ee0c1fad4e2a02e67c32e317c3c4088ea7
Randy Eckenrode <randy@largeandhighquality.com>
2020-12-01 20:56:10 -0500

    Create basic console runner

--- src/Advent2020.Console/Program.fs
+++ src/Advent2020.Console/Program.fs
@@ -1,5 +1,20 @@
 open System
 
+open Advent2020.Solutions
+
+let usage () =
+    printfn "usage: advent_2020 <day> <input>"
+    printfn "\nDays:"
+    for day in Inventory.solutions |> Map.toSeq |> Seq.map fst do
+        printfn $"   {day}"
+    -1
+
 [<EntryPoint>]
 let main argv =
-    0
+    let day = argv |> Array.tryHead |> Option.bind (fun x -> Map.tryFind x Inventory.solutions)
+    let input = argv |> Array.tryItem 1
+    match (day, input) with
+    | Some day, Some input ->
+        printfn $"Got: {day} with input: {input}"
+        0
+    | _ -> usage ()

ded70f00d226789b4042df68810c81b809362e67
Randy Eckenrode <randy@largeandhighquality.com>
2020-12-02 00:39:37 -0500

    Add liftOption to the utility functions

--- src/Advent2020.Solutions/utilities.fs
+++ src/Advent2020.Solutions/utilities.fs
@@ -2,6 +2,12 @@ module Advent2020.Solutions.Utilities
 
 open System.IO
 
+let liftOption xs =
+    Seq.foldBack (fun x xs ->
+        match xs with
+        | Some xs -> x |> Option.bind (fun o -> o::xs |> Some)
+        | None -> None) xs (Some [])
+
 let liftResult f x =
     try
         f x |> Ok

38a85fd86b3664da3802f9526699b21cc881ac7d
Randy Eckenrode <randy@largeandhighquality.com>
2020-12-02 02:43:07 -0500

    Make day 1 support both 2d and 3d hill climbing
    This is kind of hacky. I pass a value around to determine whether to disregard the third dimension or not. On the other hand, it works, and it was pretty quick to do.

--- src/Advent2020.Solutions/day1.fs
+++ src/Advent2020.Solutions/day1.fs
@@ -6,22 +6,30 @@ let name = "day1"
 
 let targetNumber = 2020
 
-let distance target (x, y) (data: array<int>) =
-    target - (data.[x] + data.[y]) |> System.Math.Abs
+let distance target (x, y, z) (data: array<int>) =
+    let zValue = data |> Array.tryItem z |> Option.orElse (Some 0) |> Option.get
+    target - (data.[x] + data.[y] + zValue) |> System.Math.Abs
 
-let enumerateNeighbors (pos&(x, y)) =
+let enumerateNeighbors (pos&(x, y, z)) =
     Seq.allPairs [x-1; x; x+1] [y-1; y; y+1]
+    |> Seq.allPairs [z-1; z; z+1]
+    |> Seq.map (fun (x, (y, z)) -> (x, y, z))
     |> Seq.filter (fun p -> p <> pos)
 
-let rec climb target pos currentDistance data =
+let isInRange bot top x =
+    x >= bot && x < top
+
+let rec climb target pos currentDistance holdZ data =
     random {
         if currentDistance = 0
         then return Some pos
         else
             let maxPos = data |> Array.length
+            let isInRange = isInRange 0 maxPos
             let possibilities =
                 enumerateNeighbors pos
-                |> Seq.filter (fun (x, y) -> x >= 0 && x < maxPos && y >= 0 && y < maxPos)
+                |> Seq.filter (fun (x, y, z) ->
+                    isInRange x && isInRange y && if holdZ then z = -1 else isInRange z)
                 |> Seq.map (fun pos -> (pos, distance target pos data))
                 |> Seq.filter (fun (_, d) -> d < currentDistance)
                 |> Array.ofSeq
@@ -30,28 +38,34 @@ let rec climb target pos currentDistance data =
             else
                 let! candidate = possibilities |> Array.sampleOne
                 let newPos, newDistance = candidate
-                return! data |> climb target newPos newDistance
+                return! data |> climb target newPos newDistance holdZ
     }
 
-let rec findSolution target numbers =
+let rec findSolution target numbers holdZ =
     random {
         let maxPos = (numbers |> Array.length) - 1
         let! startX = Statistics.uniformDiscrete (0, maxPos)
         let! startY = Statistics.uniformDiscrete (0, maxPos)
-        let start = (startX, startY)
-        match! climb target start (distance target start numbers) numbers with
+        let! startZ = if holdZ then random { return -1 } else Statistics.uniformDiscrete (0, maxPos)
+        let start = (startX, startY, startZ)
+        match! numbers |> climb target start (distance target start numbers) holdZ with
         | Some result -> return result
-        | None -> return! findSolution target numbers
+        | None -> return! findSolution target numbers holdZ
     }
 
 let run (input: seq<string>, part: string) =
+    let holdZ = part <> "part2"
     let numbers: option<list<int>> = input |> Seq.map Utilities.tryParse |> Utilities.liftOption
     match numbers with
     | None -> printfn "Error: input file not in expected format (a list of integers)."
     | Some numbers ->
         let numbers = numbers |> List.sort |> Array.ofList
         let rng = Utility.createRandomState ()
-        let result = rng |> Random.get (findSolution targetNumber numbers)
-        let n1 = numbers.[fst result]
-        let n2 = numbers.[snd result]
-        printfn $"{n1} × {n2} = {n1 * n2}"
+        let (idx1, idx2, idx3) = rng |> Random.get (findSolution targetNumber numbers holdZ)
+        let n1 = numbers.[idx1]
+        let n2 = numbers.[idx2]
+        if idx3 = -1
+        then printfn $"{n1} × {n2} = {n1 * n2}"
+        else
+            let n3 = numbers.[idx3]
+            printfn $"{n1} × {n2} × {n3} = {n1 * n2 * n3}"

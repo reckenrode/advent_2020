baba279a07782442b48d61e9ebed28340c4e80a5
Randy Eckenrode <randy@largeandhighquality.com>
2020-12-01 23:28:54 -0500

    Add a primitive command-line parser

--- src/Advent2020.Console/Program.fs
+++ src/Advent2020.Console/Program.fs
@@ -9,12 +9,19 @@ let usage () =
         printfn $"   {day}"
     -1
 
+let explainError (ex: exn) =
+    printfn $"Encountered an error while parsing the input file: {ex.Message}"
+    -1
+
+let runSolution solution input =
+    printfn $"{solution input}"
+    0
+
 [<EntryPoint>]
 let main argv =
     let day = argv |> Array.tryHead |> Option.bind (fun x -> Map.tryFind x Inventory.solutions)
-    let input = argv |> Array.tryItem 1
+    let input = argv |> Array.tryItem 1 |> Option.map Utilities.readFile
     match (day, input) with
-    | Some day, Some input ->
-        printfn $"Got: {day} with input: {input}"
-        0
+    | Some day, Some (Ok input) -> runSolution day input
+    | Some day, Some (Error ex) -> explainError ex
     | _ -> usage ()

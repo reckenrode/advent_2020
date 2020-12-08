4c89f1703f93d266d6736005fb03e4a30697b81a
Randy Eckenrode <randy@largeandhighquality.com>
2020-12-08 12:03:21 -0500

    Add day 6 part 1 implementation

---
+++ src/Advent2020.Solutions/day6.fs
@@ -0,0 +1,43 @@
+module Advent2020.Solutions.Day6
+
+open FParsec
+open FSharpx.Option
+
+let name = "day6"
+
+type Group = list<Responses>
+and Responses = Set<Answers>
+and Answers = private Answers of char
+
+module Answers =
+    let create = function
+    | ch when ch >= 'a' && ch <= 'z' -> Some (Answers ch)
+    | _ -> None
+
+module Responses =
+    let create lst =
+        maybe {
+            let! lst = lst |> List.map Answers.create |> sequence
+            return Set.ofList lst
+        }
+
+    let parser<'ex> : Parser<option<Responses>, 'ex> =
+        let questions = asciiLower
+        many1 questions |>> create
+
+module Group =
+    let countYesAnswers: Group -> int = Set.unionMany >> Set.count
+
+    let parser<'ex> : Parser<option<Group>, 'ex> =
+        sepEndBy1 Responses.parser newline |>> sequence
+
+let groupsParser<'ex> : Parser<option<list<Group>>, 'ex> =
+    sepBy1 Group.parser newline |>> sequence .>> eof
+
+let run (input: string, arg: string) =
+    match input |> run groupsParser with
+    | Failure (ex, _, _) -> printfn $"{ex}"
+    | Success (Some groups, _, _) ->
+        let n = groups |> List.map Group.countYesAnswers |> List.sum
+        printfn $"Total yes answers to unique questions summed across groups: {n}"
+    | Success (None, _, _) -> printfn "Some of the groups contained invalid data."

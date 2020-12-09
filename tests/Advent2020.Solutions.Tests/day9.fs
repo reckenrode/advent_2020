f02a23372110eab945a0489fc9fd3a6bf47c69da
Randy Eckenrode <randy@largeandhighquality.com>
2020-12-09 12:11:22 -0500

    Fix broken test

--- tests/Advent2020.Solutions.Tests/day9.fs
+++ tests/Advent2020.Solutions.Tests/day9.fs
@@ -8,9 +8,10 @@ module ``XMAS cracker`` =
     [<Fact>]
     let ``finds the first number that does not add up to the previous 5`` () =
         let input = [
-            35 ;20; 15; 25; 47; 40; 62; 55; 65; 95; 102; 117; 150; 182; 127; 219; 299; 277; 309; 576
+            35L; 20L; 15L; 25L; 47L; 40L; 62L; 55L; 65L; 95L; 102L;
+            117L; 150L; 182L; 127L; 219L; 299L; 277L; 309L; 576L
         ]
         let window = 5
-        let expected = Some 127
+        let expected = Some 127L
         let result = input |> XmasCracker.findNonSumming window
         result |> should equal expected

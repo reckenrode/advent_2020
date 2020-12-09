b22c1eaca1d6e8b516666aa709fb34f55eb0dc5b
Randy Eckenrode <randy@largeandhighquality.com>
2020-12-08 22:02:21 -0500

    Refactor set of indices

--- src/Advent2020.Solutions/day7.fs
+++ src/Advent2020.Solutions/day7.fs
@@ -5,13 +5,15 @@ open FSharpx.Option
 open Utilities
 
 module BagSolver =
+    let private setOfIndices (s, idx) x =
+        if x <> 0
+        then (s |> Set.add idx, idx + 1)
+        else (s, idx + 1)
+
     let canContain graph bag =
         let rec canContain' graph seen bag =
             let row = Array2D.rowSpan bag graph
-            let (newSeen, _) = Span.fold (fun (s, idx) x ->
-                if x <> 0
-                then (s |> Set.add idx, idx + 1)
-                else (s, idx + 1)) (Set.empty, 0) row
+            let (newSeen, _) = Span.fold setOfIndices (Set.empty, 0) row
             if seen |> Set.isSubset newSeen
             then seen
             else

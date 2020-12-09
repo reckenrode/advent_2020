72e2bbe3929cb85dcb855491fe10c4801a6b90a2
Randy Eckenrode <randy@largeandhighquality.com>
2020-12-09 08:58:39 -0500

    Add day 9 part 1 implementation

---
+++ src/Advent2020.Solutions/day9.fs
@@ -0,0 +1,36 @@
+module Advent2020.Solutions.Day9
+
+open FSharpx
+open FSharpx.Collections
+
+module XmasCracker =
+    let rec private calculateSums window = function
+    | [] -> LazyList.empty
+    | x::xs ->
+        let xSums = xs |> Seq.truncate (window - 1) |> Seq.map ((+) x)
+        LazyList.consDelayed xSums (fun () -> (xs |> calculateSums window))
+
+    let findNonSumming window data =
+        let rest = data |> Seq.skip window
+        let sums = data |> calculateSums window
+        let rec findNonSumming' window data sums =
+            match data |> Seq.unCons with
+            | Some (x, xs) ->
+                let windowSums = sums |> LazyList.take window |> Seq.tryFind (Seq.tryFind ((=) x) >> Option.isSome)
+                if windowSums |> Option.isNone
+                then Some x
+                else findNonSumming' window xs (sums |> LazyList.tail)
+            | _ -> None
+        findNonSumming' window rest sums
+
+let name = "day9"
+
+let run (input: seq<string>, arg: string) =
+    match input |> Seq.map Utilities.tryParse |> List.ofSeq |> Option.sequence with
+    | None -> printfn "error reading list of numbers"
+    | Some (nums: list<int64>) ->
+        match nums |> XmasCracker.findNonSumming 25 with
+        | None -> printfn "All the numbers are good. No bad number found."
+        | Some badNum ->
+            printfn $"The bad number is {badNum}"
+

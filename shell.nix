4b1eee4e7b0ff411446449a52dd0e1302acd6b76
Randy Eckenrode <randy@largeandhighquality.com>
2020-12-10 12:53:41 -0500

    Just assume the Nix environment is sensible

--- shell.nix
+++ shell.nix
@@ -1,6 +1,5 @@
 let
-  # As of Winter 2020, .NET 5.0 is only available in the unstable channel
-  pkgs = import (fetchTarball https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz) {};
+  pkgs = import <nixpkgs> {};
 in
 pkgs.mkShell {
   buildInputs = with pkgs; [

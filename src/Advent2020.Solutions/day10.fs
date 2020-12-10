7643dd16ed61d365b5af38ca89a532e544bd0b48
Randy Eckenrode <randy@largeandhighquality.com>
2020-12-10 08:43:08 -0500

    add day 10 part 1 solution

--- src/Advent2020.Solutions/day10.fs
+++ src/Advent2020.Solutions/day10.fs
@@ -1,5 +1,7 @@
 module Advent2020.Solutions.Day10
 
+open FSharpx
+
 module AdapterAnalyzer =
     let createChain socketJoltage adapters =
         match adapters |> List.sort with
@@ -16,3 +18,18 @@ module AdapterAnalyzer =
         |> Seq.map (fun (lhs, rhs) -> rhs - lhs)
         |> Seq.countBy id
         |> Map.ofSeq
+
+let name = "day10"
+
+let run (input: seq<string>, arg: string) =
+    match input |> Seq.map Utilities.tryParse |> List.ofSeq |> Option.sequence with
+    | None -> printfn "Error: failed to read input file"
+    | Some adapters ->
+        match adapters |> AdapterAnalyzer.createChain 0 with
+        | None -> printfn "Error: adapters were not capable of making a chain (bad input?)"
+        | Some chain ->
+            let statistics = chain |> AdapterAnalyzer.findDifferences 0
+            let oneCount = statistics |> Map.tryFind 1 |> Option.getOrElse 0
+            let threeCount = statistics |> Map.tryFind 3 |> Option.getOrElse 0
+            printfn $"The distribution is: {statistics}"
+            printfn $"The product of the 1- and 3-counts is: {oneCount * threeCount}"

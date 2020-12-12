d48db3e56dbb0da26d8a7d1c09b9f4d53405ac8e
Randy Eckenrode <randy@largeandhighquality.com>
2020-12-11 22:40:07 -0500

    Refactor part 1
    Ideally we could just create a new function, but that doesn’t work well in Rust. Instead, create a function that runs the filter and is paramertized appropriately.

--- src/solutions/day11/part1.rs
+++ src/solutions/day11/part1.rs
@@ -1,28 +1,39 @@
 use super::waiting_area::*;
 
-pub fn nearby_filter(grid: &mut [u8], width: usize, height: usize) {
-    let mut new_grid = Vec::new();
-    new_grid.resize(width * height, FLOOR);
-    for (row_index, row) in new_grid.chunks_mut(width).enumerate() {
-        for column_index in 0 .. width {
-            let neighbors = neighbors(row_index, column_index);
-            let tally = neighbors.iter()
-                .filter(|(r, c)| *r < height && *c < width)
-                .fold(0, |sum, (r, c)| {
-                    sum + if grid[r * width + c] == PERSON { 1 } else { 0 }
-                });
-            let current_cell = grid[row_index * width + column_index];
-            row[column_index] = match current_cell {
-                PERSON if tally >= 4 => SEAT,
-                SEAT   if tally == 0 => PERSON,
-                _ => current_cell
+pub (in crate::solutions::day11) fn run_filter(
+    grid: &mut [u8], width: usize, height: usize,
+    neighbors: impl Fn(&[u8], usize, usize) -> [(usize, usize); 8],
+    evaluate_tally: impl Fn(i32, u8) -> u8) {
+        let mut new_grid = Vec::new();
+        new_grid.resize(width * height, FLOOR);
+        for (row_index, row) in new_grid.chunks_mut(width).enumerate() {
+            for column_index in 0 .. width {
+                let neighbors = neighbors(grid, row_index, column_index);
+                let tally = neighbors.iter()
+                    .filter(|(r, c)| *r < height && *c < width)
+                    .fold(0, |sum, (r, c)| {
+                        sum + if grid[r * width + c] == PERSON { 1 } else { 0 }
+                    });
+                let current_cell = grid[row_index * width + column_index];
+                row[column_index] = evaluate_tally(tally, current_cell)
             }
         }
-    }
-    grid.copy_from_slice(&new_grid);
+        grid.copy_from_slice(&new_grid);
 }
 
-fn neighbors(row: usize, column: usize) -> [(usize, usize); 8] {
+pub fn nearby_filter(grid: &mut [u8], width: usize, height: usize) {
+    run_filter(grid, width, height, neighbors, evaluate_tally)
+}
+
+fn evaluate_tally(tally: i32, current_cell: u8) -> u8 {
+    match current_cell {
+        PERSON if tally >= 4 => SEAT,
+        SEAT   if tally == 0 => PERSON,
+        _ => current_cell
+    }
+}
+
+fn neighbors(_grid: &[u8], row: usize, column: usize) -> [(usize, usize); 8] {
     [
         (row.wrapping_sub(1),   column.wrapping_sub(1)),
         (row,                   column.wrapping_sub(1)),

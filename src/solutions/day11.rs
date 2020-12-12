1b0fcb1da6723756120f27d8f7e4b093bbcf1772
Randy Eckenrode <randy@largeandhighquality.com>
2020-12-11 21:40:47 -0500

    Move day11 unit tests to the top-level module

--- src/solutions/day11.rs
+++ src/solutions/day11.rs
@@ -43,37 +43,34 @@ impl Solution {
 
 #[cfg(test)]
 mod tests {
-    use super::{waiting_area::*, *};
+    use super::*;
 
-    mod waiting_process {
-        use super::*;
-        #[test]
-        fn the_waiting_area_stabilizes() {
-            let initial_room = "\
-                L.LL.LL.LL\n\
-                LLLLLLL.LL\n\
-                L.L.L..L..\n\
-                LLLL.LL.LL\n\
-                L.LL.LL.LL\n\
-                L.LLLLL.LL\n\
-                ..L.L.....\n\
-                LLLLLLLLLL\n\
-                L.LLLLLL.L\n\
-                L.LLLLL.LL";
-            let expected_area_contents = "\
-                #.#L.L#.##\n\
-                #LLL#LL.L#\n\
-                L.#.L..#..\n\
-                #L##.##.L#\n\
-                #.#L.LL.LL\n\
-                #.#L#L#.##\n\
-                ..L.L.....\n\
-                #L#L##L#L#\n\
-                #.LLLLLL.L\n\
-                #.#L#L#.##";
-            let mut waiting_area = WaitingArea::parse(initial_room).unwrap();
-            waiting_area.wait_until_stable();
-            assert_eq!(waiting_area.to_string(), expected_area_contents);
-        }
+    #[test]
+    fn the_waiting_area_stabilizes() {
+        let initial_room = "\
+            L.LL.LL.LL\n\
+            LLLLLLL.LL\n\
+            L.L.L..L..\n\
+            LLLL.LL.LL\n\
+            L.LL.LL.LL\n\
+            L.LLLLL.LL\n\
+            ..L.L.....\n\
+            LLLLLLLLLL\n\
+            L.LLLLLL.L\n\
+            L.LLLLL.LL";
+        let expected_area_contents = "\
+            #.#L.L#.##\n\
+            #LLL#LL.L#\n\
+            L.#.L..#..\n\
+            #L##.##.L#\n\
+            #.#L.LL.LL\n\
+            #.#L#L#.##\n\
+            ..L.L.....\n\
+            #L#L##L#L#\n\
+            #.LLLLLL.L\n\
+            #.#L#L#.##";
+        let mut waiting_area = WaitingArea::parse(initial_room).unwrap();
+        waiting_area.wait_until_stable();
+        assert_eq!(waiting_area.to_string(), expected_area_contents);
     }
 }

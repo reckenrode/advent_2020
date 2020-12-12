006087d612b6a83be7c291e06e563d4f89bd5ee0
Randy Eckenrode <randy@largeandhighquality.com>
2020-12-12 14:46:36 -0500

    Add simple, translational movement

--- src/solutions/day12/ship.rs
+++ src/solutions/day12/ship.rs
@@ -1,10 +1,21 @@
 pub struct Ship {
+    position: (f64, f64),
+}
 
+pub enum Action {
+    MoveNorth(u32),
+    MoveSouth(u32),
+    MoveEast(u32),
+    MoveWest(u32),
 }
 
 impl Ship {
     pub fn new() -> Self {
-        Ship {}
+        Ship { position: (0.0, 0.0) }
+    }
+
+    pub fn act(&mut self, action: Action) {
+        action.apply(self)
     }
 
     pub fn orientation(&self) -> f64 {
@@ -12,7 +23,18 @@ impl Ship {
     }
 
     pub fn position(&self) -> (f64, f64) {
-        (0.0, 0.0)
+        self.position
+    }
+}
+
+impl Action {
+    fn apply(&self, ship: &mut Ship) {
+        match self {
+            Self::MoveNorth(dy) => ship.position = (0.0, *dy as f64),
+            Self::MoveSouth(dy) => ship.position = (0.0, -(*dy as f64)),
+            Self::MoveEast(dx) => ship.position = (*dx as f64, 0.0),
+            Self::MoveWest(dx) => ship.position = (-(*dx as f64), 0.0),
+        }
     }
 }
 
@@ -33,16 +55,40 @@ mod tests {
         let ship = Ship::new();
         assert_eq!(ship.orientation(), expected_orientation);
     }
+
+    use proptest::prelude::*;
+
+    proptest! {
+        #[test]
+        fn when_the_action_is_north_the_ship_coordinates_change_by_the_specified_value(dist: u32) {
+            let expected_position = (0.0, dist as f64);
+            let mut ship = Ship::new();
+            ship.act(Action::MoveNorth(dist));
+            prop_assert_eq!(ship.position(), expected_position);
+        }
+
+        #[test]
+        fn when_the_action_is_south_the_ship_coordinates_change_by_the_specified_value(dist: u32) {
+            let expected_position = (0.0, -(dist as f64));
+            let mut ship = Ship::new();
+            ship.act(Action::MoveSouth(dist));
+            prop_assert_eq!(ship.position(), expected_position);
+        }
+
+        #[test]
+        fn when_the_action_is_east_the_ship_coordinates_change_by_the_specified_value(dist: u32) {
+            let expected_position = (dist as f64, 0.0);
+            let mut ship = Ship::new();
+            ship.act(Action::MoveEast(dist));
+            prop_assert_eq!(ship.position(), expected_position);
+        }
+
+        #[test]
+        fn when_the_action_is_west_the_ship_coordinates_change_by_the_specified_value(dist: u32) {
+            let expected_position = (-(dist as f64), 0.0);
+            let mut ship = Ship::new();
+            ship.act(Action::MoveWest(dist));
+            prop_assert_eq!(ship.position(), expected_position);
+        }
+    }
 }
-
-    // use proptest::prelude::*;
-
-    // proptest! {
-    //     #[test]
-    //     fn when_the_action_is_north_the_ship_coordinates_change_by_the_specified_value(dist: u32) {
-    //         let expectedPosition = (0, dist);
-    //         let ship = Ship::new();
-    //         ship.move(Action(dist));
-    //         prop_assert_eq!(ship.position, expectedPosition);
-    //     }
-    // }

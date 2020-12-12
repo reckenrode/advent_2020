8ca78e8c658516411ee829db5d30d222d2b94030
Randy Eckenrode <randy@largeandhighquality.com>
2020-12-11 21:19:30 -0500

    Implement waiting until the room stabilizes

--- src/solutions/day11.rs
+++ src/solutions/day11.rs
@@ -44,6 +44,22 @@ fn four_near_filter(grid: &mut [u8], width: usize, height: usize) {
     }
     grid.copy_from_slice(&new_grid);
 }
+pub trait Day11Extensions {
+    fn wait_until_stable(&mut self);
+}
+
+impl Day11Extensions for waiting_area::WaitingArea {
+    fn wait_until_stable(&mut self) {
+        let mut current_area = self.to_string();
+        let mut new_area;
+        self.apply_rules(four_near_filter);
+        while { new_area = self.to_string(); current_area != new_area } {
+            current_area = new_area;
+            self.apply_rules(four_near_filter);
+        }
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -65,6 +81,38 @@ mod tests {
             let expected = "#.L#\n#LL#";
             waiting_area.apply_rules(four_near_filter);
             assert_eq!(waiting_area.to_string(), expected);
+        }
+    }
+
+    mod waiting_process {
+        use super::*;
+        #[test]
+        fn the_waiting_area_stabilizes() {
+            let initial_room = "\
+                L.LL.LL.LL\n\
+                LLLLLLL.LL\n\
+                L.L.L..L..\n\
+                LLLL.LL.LL\n\
+                L.LL.LL.LL\n\
+                L.LLLLL.LL\n\
+                ..L.L.....\n\
+                LLLLLLLLLL\n\
+                L.LLLLLL.L\n\
+                L.LLLLL.LL";
+            let expected_area_contents = "\
+                #.#L.L#.##\n\
+                #LLL#LL.L#\n\
+                L.#.L..#..\n\
+                #L##.##.L#\n\
+                #.#L.LL.LL\n\
+                #.#L#L#.##\n\
+                ..L.L.....\n\
+                #L#L##L#L#\n\
+                #.LLLLLL.L\n\
+                #.#L#L#.##";
+            let mut waiting_area = WaitingArea::parse(initial_room).unwrap();
+            waiting_area.wait_until_stable();
+            assert_eq!(waiting_area.to_string(), expected_area_contents);
         }
     }
 }

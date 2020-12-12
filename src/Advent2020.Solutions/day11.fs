8900f4b75f86a51d85c7edb96fe80556dd058ff8
Randy Eckenrode <randy@largeandhighquality.com>
2020-12-11 23:43:23 -0500

    Refactor nearbyFilter

--- src/Advent2020.Solutions/day11.fs
+++ src/Advent2020.Solutions/day11.fs
@@ -7,26 +7,31 @@ let private rcFromList = function
 | r::c::_ -> (r, c)
 | _ -> failwith "expected two elements but got something else. This should not happen."
 
-let nearbyFilter grid =
+let mkFilter neighbors evaluateTally grid =
     let width = grid |> Array2D.width
     let height = grid |> Array2D.height
-    let neighbors (r, c) =
-        Day1.enumerateNeighbors [r; c]
-        |> Seq.map rcFromList
-        |> Seq.filter (fun (r, c) -> r >= 0 && c >= 0 && r < height && c < width)
-        |> Seq.map (fun (r, c) -> grid.[r, c])
     Array2D.init height width <| fun r c ->
         let currentCell = grid.[r, c]
         if currentCell = '.'
         then currentCell
         else
             let occupied =
-                neighbors (r, c)
+                neighbors (r, c) height width grid
                 |> Seq.fold (fun ocp cell -> if cell = '#' then ocp + 1 else ocp) 0
-            match currentCell with
-            | '#' when occupied >= 4 -> 'L'
-            | 'L' when occupied = 0 -> '#'
-            | _ -> currentCell
+            evaluateTally occupied currentCell
+
+let nearbyFilter =
+    mkFilter
+        (fun (r, c) height width grid ->
+            Day1.enumerateNeighbors [r; c]
+            |> Seq.map rcFromList
+            |> Seq.filter (fun (r, c) -> r >= 0 && c >= 0 && r < height && c < width)
+            |> Seq.map (fun (r, c) -> grid.[r, c]))
+        (fun tally cell ->
+            match cell with
+            | '#' when tally >= 4 -> 'L'
+            | 'L' when tally = 0 -> '#'
+            | _ -> cell)
 
 let rec waitUntilStable f area =
     let result = area |> WaitingArea.applyRules f

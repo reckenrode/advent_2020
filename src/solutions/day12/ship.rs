2d971cae474f36f8df1c12bca9c7c4f6eafa555d
Randy Eckenrode <randy@largeandhighquality.com>
2020-12-12 14:20:16 -0500

    Add a basic ship type

--- src/solutions/day12/ship.rs
+++ src/solutions/day12/ship.rs
@@ -22,16 +22,4 @@ mod tests {
         let ship = Ship::new();
         assert_eq!(ship.position(), expected_position);
     }
-
-    // use proptest::prelude::*;
-
-    // proptest! {
-    //     #[test]
-    //     fn when_the_action_is_north_the_ship_coordinates_change_by_the_specified_value(dist: u32) {
-    //         let expectedPosition = (0, dist);
-    //         let ship = Ship::new();
-    //         ship.move(Action(dist));
-    //         prop_assert_eq!(ship.position, expectedPosition);
-    //     }
-    // }
 }

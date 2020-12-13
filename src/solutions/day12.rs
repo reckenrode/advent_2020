75733bb0bb5836b8963e8efe8dd4bca90313d686
Randy Eckenrode <randy@largeandhighquality.com>
2020-12-12 20:17:15 -0500

    Add day 12 part 1 solution

--- src/solutions/day12.rs
+++ src/solutions/day12.rs
@@ -1,1 +1,27 @@
+use clap::Clap;
+
+mod part1;
 mod ship;
+
+use ship::{Action, Ship};
+
+#[derive(Clap)]
+pub struct Solution {
+    input: std::path::PathBuf,
+}
+
+impl Solution {
+    pub fn run(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let data = std::fs::read_to_string(&self.input)?;
+        let lines: Result<Vec<Action>, anyhow::Error> = data.lines()
+            .map(Action::parse)
+            .collect();
+        let mut ship = Ship::new();
+        for line in lines? {
+            ship.act(line);
+        }
+        let distance = part1::manhattan_distance(ship.position());
+        println!("The manhattan distance is {}.", distance);
+        Ok(())
+    }
+}

280556018f53f13701a743b7a2c020a69daeadc2
Randy Eckenrode <randy@largeandhighquality.com>
2020-12-12 20:42:58 -0500

    Add day12 to the runner

--- src/solutions.rs
+++ src/solutions.rs
@@ -6,13 +6,15 @@ mod day12;
 
 #[derive(Clap)]
 pub enum Solution {
-    Day11(day11::Solution)
+    Day11(day11::Solution),
+    Day12(day12::Solution),
 }
 
 impl Solution {
     pub fn run(self) -> Result<(), Box<dyn Error>> {
         match self {
-            Solution::Day11(solution) => solution.run()
+            Solution::Day11(solution) => solution.run(),
+            Solution::Day12(solution) => solution.run(),
         }
     }
 }
